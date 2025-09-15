e94af29478f9f9d9d549af610ec56fd073f2cfa9
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2025-09-15 11:38:24 -0500

    get decks based on id

--- FlashCardsV5/app/decks/decks.tsx
+++ FlashCardsV5/app/decks/decks.tsx
@@ -1,10 +1,28 @@
 import { View, Text, Button } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { supabase } from "../utils/supabase";
 import { ThemedText } from "@/components/ThemedText";
+import { useUser } from "../context/userContext"
 
 export default function Decks() {
+    const { userId, setUserId } = useUser();
+
+    useEffect(() => {
+        getDecks();
+    }, [userId])
+
+    async function getDecks() {
+        console.log("getting decks")
+        const data = await supabase
+        .from("deck")
+        .select("deck_name")
+        .eq("user_id", userId)
+
+        console.log(data)
+
+    }
+
   return (
     <SafeAreaView>
       <ThemedText>Decks</ThemedText>
